b7d09be2a7ac99aed1ce7e00d6119bcb5564dae3
hoonkim <hoonk9387@gmail.com>
2022-08-26 18:01:11 -0500

    Completed assignment.

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,17 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  # Action
+  def show
+    # byebug
+
+    # talk to Model
+    # Find by id attribute
+    cheese = Cheese.find(params[:id])
+    # Render JSON
+    render json: cheese
+    # byebug
+
+  end
+
 end
